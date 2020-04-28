3466049001761aa11befd719f6c2347e0beb04bb
miqbalulh <iqbalul.hidayat2801@gmail.com>
2020-04-28 09:41:48 +0700

    Change Device

--- app/src/main/java/com/example/whitecube/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/whitecube/Fragment/HomeFragment.kt
@@ -1,9 +1,13 @@
 package com.example.whitecube.Fragment
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Typeface
+import android.os.AsyncTask
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,6 +31,8 @@ import com.example.whitecube.Relay.AddRelay
 import com.example.whitecube.Relay.DetailRelay
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.*
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_ganti_device.*
 import kotlinx.android.synthetic.main.activity_register_user.*
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -34,6 +40,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
 class HomeFragment : Fragment() {
 
 
+    private lateinit var suhu : TextView
     private lateinit var namaDevice : TextView
     private lateinit var btn_add : ImageView
     private lateinit var tambahPerangkat : ImageView
@@ -47,18 +54,21 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view : View = inflater.inflate(R.layout.fragment_home, container, false)
         SP = activity!!.getSharedPreferences("WhiteCube", Context.MODE_PRIVATE)
         refrelay = FirebaseDatabase.getInstance().getReference("relay")
         refdevice = FirebaseDatabase.getInstance().getReference("device")
         rvRelay = view.findViewById(R.id.rvRelay)
         relays = arrayListOf()
+        suhu = view.findViewById(R.id.suhu)
         tambahPerangkat = view.findViewById(R.id.tambahPerangkat)
         btn_add = view.findViewById(R.id.btn_add)
         namaDevice = view.findViewById(R.id.namaDevice)
 
+
         // set content
+
+
         namaDevice.text = SP.getString("namaDevice","").toString()
         if(SP.getString("modeUser","").toString()=="1"){
             tambahPerangkat.visibility = View.VISIBLE
@@ -115,6 +125,8 @@ class HomeFragment : Fragment() {
 
         })
     }
+
+
 
 
 }
